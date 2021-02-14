6579ed5b228859a8a010bb636990376f6ca54ddb
Artem Markin <artem@floryn.com>
2021-02-14 17:30:53 +0100

    fix data reading

--- src/etl.py
+++ src/etl.py
@@ -1,25 +1,85 @@
 import logging
 import pathlib
-from dataclasses import InitVar, dataclass
+from dataclasses import InitVar, dataclass, field
 from functools import wraps
 from typing import Callable
 
+import numpy as np
 import pandas as pd
 
 
-def decorate_df(func: Callable):
+def _decorate_df(func: Callable):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        logger.log(f"[{func.__name__}]: Input Data Shape={args[0].shape}")
-        logger.log(level, f"[DONE], Resultind Data Shape={func(*args, **kwargs).shape}")
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+
+        func_result = func(*args, **kwargs)
+        logging.info(
+            f" {func.__name__} [DONE], Resulting Data Shape={func_result.shape}"
+        )
+        return func_result
+
+    return wrapper
 
 
 @dataclass
 class SugarData:
+    """
+    Data class which preprocess and store measurement of the glucose in blood.
+    """
 
     data_path: InitVar[pathlib.Path]
-    data: pd.DataFrame = field(init=False)
+    raw: pd.DataFrame = field(init=False)
+    cleaned: pd.DataFrame = field(init=False)
+    glucose: pd.DataFrame = field(init=False)
+    notes: pd.DataFrame = field(init=False)
+
     def __post_init__(self, data_path: pathlib.Path):
-        self.data_path = data_path
-        self.data = __read_raw_data(self)
-        self.data_cleaned = __clean_data(self)
+        self.raw = self.__read_data(data_path)
+        self.cleaned = self.__clean_data()
+        self.glucose = self.__glucose()
+        self.notes = self.__notes()
+
+    @_decorate_df
+    def __read_data(self, data_path: pathlib.Path) -> pd.DataFrame:
+        return pd.read_csv(data_path, skiprows=1)
+
+    @_decorate_df
+    def __clean_data(self) -> pd.DataFrame:
+        data = (
+            self.raw.rename(columns={"Notes": "notes"})
+            .assign(
+                # get glucose level from manual and automatic scans
+                glucose_level=lambda d: np.where(
+                    d["Historic Glucose mmol/L"].isnull(),
+                    d["Scan Glucose mmol/L"],
+                    d["Historic Glucose mmol/L"],
+                ),
+                datetime=lambda d: pd.to_datetime(
+                    d["Device Timestamp"], format="%d-%m-%Y %H:%M"
+                ),
+            )
+            .loc[
+                lambda d: (d["glucose_level"].notnull()) | (d["notes"].notnull()),
+                [
+                    "datetime",
+                    "glucose_level",
+                    "notes",
+                ],
+            ]
+            .sort_values(by="datetime")
+            .reset_index(drop=True)
+        )
+
+        return data
+
+    @_decorate_df
+    def __glucose(self) -> pd.DataFrame:
+        return self.cleaned.loc[
+            lambda d: d["glucose_level"].notnull(), ["datetime", "glucose_level"]
+        ]
+
+    @_decorate_df
+    def __notes(self) -> pd.DataFrame:
+        return self.cleaned.loc[lambda d: d["notes"].notnull(), ["datetime", "notes"]]
